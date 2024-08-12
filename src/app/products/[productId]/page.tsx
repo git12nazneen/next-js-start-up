56edd34bf975c0d7dd0b9d7b0724a4c8b73ff752
git12nazneen <lipinazneen65@gmail.com>
2024-08-12 19:59:28 +0600

    added dynamic routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function productDetails({params}) {
+  return (
+    <div>productDetails {params.productId}</div>
+  )
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ProductList() {
+  return (
+    <div>ProductList
+        <p>Product 1</p>
+        <p>Product 2</p>
+        <p>Product 3</p>
+    </div>
+    
+  )
+}
