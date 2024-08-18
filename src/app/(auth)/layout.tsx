852cfb1a156f5c04deeccdf6b85cf761d5ca807b
git12nazneen <lipinazneen65@gmail.com>
2024-08-18 19:54:32 +0600

    activ link

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+
+const navLinks = [
+    {name:"Register", href:'/register'},
+    {name:"Login", href:'/login'},
+    {name:"Forgot PassWord", href:'/forgotPassword'},
+]
+
+export default function AuthLayout({
+    children,
+}:{
+    children:React.ReactNode;
+}) {
+    const pathname = usePathname()
+  return (
+    <div>
+        {navLinks.map((link)=>{
+            const isActive =  pathname.startsWith(link.href)
+        return(
+            <Link href={link.href} key={link.name}
+            className={isActive? 'font-bold mr-2 text-red-400' : 'font-light text-green-300 mr-2'}>
+            {link.name}
+            </Link>
+        )
+    })}
+    {children}
+    </div>
+  )
+}
