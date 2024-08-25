ddec7273a02d2869c4746c4cd39458344729e05c
git12nazneen <lipinazneen65@gmail.com>
2024-08-25 15:23:23 +0600

    added delete and query params

--- src/comments/[id]/route.ts
+++ src/comments/[id]/route.ts
@@ -15,11 +15,24 @@ export async function PATCH(
   { params }: { params: { id: string } }
 );
 {
-    const body = await request.json();
-    const {text }= body;
-    const index = comment.findIndex(
-        (comment) => comment.id === parseInt(params.id)
-    ) 
-    comments[index].text = text;
-    return Response.json(comments[index])
+  const body = await request.json();
+  const { text } = body;
+  const index = comment.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  comments[index].text = text;
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+);
+{
+    const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+    const deletedComment = comments[index];
+    comments.splice(index,1)
+    return Response.json(deletedComment);
 }
--- src/comments/route.ts
+++ src/comments/route.ts
@@ -1,7 +1,12 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
+import { comment } from "postcss";
 
-export async function GET() {
-  return new Response.json(comments);
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get('query');
+  const filteredComments = query? comments.filter((comment) => comment.text.includes(query)) : comments
+  return new Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
