1913a7d603d2622b070414be009313c625ac5abf
git12nazneen <lipinazneen65@gmail.com>
2024-08-25 14:15:43 +0600

    added patch

--- src/comments/[id]/route.ts
+++ src/comments/[id]/route.ts
@@ -3,7 +3,23 @@ export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
-    const comment = comments.find((comment) => comment.id === parseInt(params.id));
- 
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+
   return new Response("GET Handlet");
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+);
+{
+    const body = await request.json();
+    const {text }= body;
+    const index = comment.findIndex(
+        (comment) => comment.id === parseInt(params.id)
+    ) 
+    comments[index].text = text;
+    return Response.json(comments[index])
 }
